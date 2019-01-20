d27f1e14b1e0203228ef6879a493a8caf6c8029a
Jannis Konopka <jannis.konopka@gmx.de>
2019-01-20 21:08:30 +0100

    created some decent train and test sets with an accuracy of 85 percent on the test set with 150 epochs and use of contant Q Spectrograms

--- TensorRecordGenerator.py
+++ TensorRecordGenerator.py
@@ -34,7 +34,7 @@ def load_image(addr):
 
 
 def storeTrainData(train_addrs, train_labels):
-    train_filename = 'train.tfrecordsTEST'  # address to save the TFRecords file
+    train_filename = 'train.tfrecords'  # address to save the TFRecords file
     # open the TFRecords file
     writer = tf.python_io.TFRecordWriter(train_filename)
     for i in range(len(train_addrs)):
@@ -58,11 +58,73 @@ def storeTrainData(train_addrs, train_labels):
     sys.stdout.flush()
 
 
-def generateTrainData():
+def generateTrainData(path = ('../trainSet1/constQSpectrograms/*.png')):
     shuffle_data = True  # shuffle the addresses before saving
-    spectrograms_train_path = '../trainSet1/constQSpectrograms/*.png'
+
+    addrs = []
+    labels = []
+
+    for spectrograms_train_path in path:
+        print(spectrograms_train_path)
+        # read addresses and labels from the 'train' folder
+        addrs.extend(glob.glob(spectrograms_train_path))
+        labels = []
+        for addr in addrs:
+            baseName = os.path.basename(addr)
+            pureName = os.path.splitext(baseName)[0]
+            #print(str)
+
+            numbersInPureName = [int(s) for s in pureName.split('_') if s.isdigit()]
+            label = numbersInPureName[0]
+            labels.append(label)
+            #print(label)
+
+        #print(labels)
+        #print("\n") 
+        #print(addrs)
+
+
+    # to shuffle data
+    if shuffle_data:
+        c = list(zip(addrs, labels))
+        shuffle(c)
+        addrs, labels = zip(*c)
+
+
+    storeTrainData(addrs, labels)
+
+
+
+def addTestData(test_addrs, test_labels):
+    test_filename = 'test.tfrecords'  # address to save the TFRecords file
+    # open the TFRecords file
+    writer = tf.python_io.TFRecordWriter(test_filename)
+    for i in range(len(test_addrs)):
+    # print how many images are saved every 1000 images
+        if not i % 10:
+            print('Test data: {}/{}'.format(i, len(test_addrs)))
+            sys.stdout.flush()
+        # Load the image
+        img = load_image(test_addrs[i])
+        label = test_labels[i]
+        # Create a feature
+        feature = {'test/label': _int64_feature(label),
+                   'test/image': _bytes_feature(tf.compat.as_bytes(img.tostring()))}
+        # Create an example protocol buffer
+        example = tf.train.Example(features=tf.train.Features(feature=feature))
+
+        # Serialize to string and write on the file
+        writer.write(example.SerializeToString())
+
+    writer.close()
+    sys.stdout.flush()
+
+
+def generateTestData(path='../trainSet1/constQSpectrograms/*.png'):
+    shuffle_data = False  # shuffle the addresses before saving
+    spectrograms_test_path = path
     # read addresses and labels from the 'train' folder
-    addrs = glob.glob(spectrograms_train_path)
+    addrs = glob.glob(spectrograms_test_path)
     labels = []
     for addr in addrs:
         baseName = os.path.basename(addr)
@@ -86,12 +148,14 @@ def generateTrainData():
         addrs, labels = zip(*c)
 
 
-    storeTrainData(addrs, labels)
+    addTestData(addrs, labels)
 
 
 
+generateTrainData(('../trainSet5/constQSpectrograms/*.png', '../trainSet6/constQSpectrograms/*.png', '../trainSet7/constQSpectrograms/*.png', '../trainSet8/constQSpectrograms/*.png', '../trainSet9/constQSpectrograms/*.png'))#, '../trainSet6/pseudoQSpectrograms/*.png'))
+generateTestData('../trainSet10/constQSpectrograms/*.png')
 
-generateTrainData()
+#generateTrainData()
 
 
 
